88e5bbb2434b9a0e4940ef13af8a8f872d8a8907
Kristal <kristalkung@gmail.com>
2021-02-03 21:40:26 +0000

    Made names of melons as the keys

--- melons.py
+++ melons.py
@@ -1,8 +1,8 @@
 melon_names = {
-    1: ('Honeydew', 0.99, True, None, None, None),
-    2: ('Crenshaw', 2.00, False, None, None, None),
-    3: ('Crane', 2.50, False, None, None, None),
-    4: ('Casaba', 2.50, False, None, None, None),
-    5: ('Cantaloupe', 0.99, False, None, None, None),
+    'Honeydew': (0.99, True, None, None, None),
+    'Crenshaw': (2.00, False, None, None, None),
+    'Crane': (2.50, False, None, None, None),
+    'Casaba': (2.50, False, None, None, None),
+    'Cantaloupe': (0.99, False, None, None, None),
 }
-# values: name, price, seedlessness, flesh color, rind color, weight
+# keys: name; values: price, seedlessness, flesh color, rind color, weight
